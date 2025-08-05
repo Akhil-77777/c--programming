ce81fb74c10cf943299509ec63978d250a861626
Akhil-77777 <megharajuakhilkumar@gmail.com>
2025-08-05 09:49:27 +0530

    Add files via upload

---
+++ arthematic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+
+{
+int a, b, sum, diff,pro,quo;
+scanf ("%d%d", &a, &b) ;
+
+sum=a+b;
+diff=a-b;
+pro=a*b;
+quo=a/b;
+
+printf("\n sum: %d", sum) ;
+printf("\n difference:%d",diff);
+printf ("\n product : d",pro) ;
+printf("\n quotient:%d",quo);
+  return 0;
+}
